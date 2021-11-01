58da88a31c45553bee91ce446768d72866d88057
maxarvid <max.arvid.anderson@gmail.com>
2021-11-01 17:51:46 +0100

    adds spec to check if image is returned at index action

--- spec/requests/api/articles_index_spec.rb
+++ spec/requests/api/articles_index_spec.rb
@@ -29,6 +29,12 @@ RSpec.describe 'GET /api/articles', type: :request do
       it 'is expected to return the author of the article' do
         expect(response_json['articles'].last['authors'].last['name']).to eq journalist.name
       end
+
+      it 'is expected to return an image with the article' do
+        binding.pry
+        expect(response_json['articles'].last).to include 'image'
+      end
+      
     end
 
     describe 'search for articles by categories' do
