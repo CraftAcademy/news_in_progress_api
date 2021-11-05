3a63fb2f6bf071e05f9a7868697c601331da97f3
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-05 08:53:06 +0100

    adds new article to seed

--- db/seeds/002_users.rb
+++ db/seeds/002_users.rb
@@ -4,3 +4,5 @@ User.create(name: 'Becky Waterton', email: 'becky@nip.com', role: 2, password: '
 User.create(name: 'Pascale Hughes', email: 'pascale@nip.com', role: 2, password: 'password')
 User.create(name: 'Fraser Hughes', email: 'fraser@nip.com', role: 3, password: "password")
 User.create(name: 'Matthew Henry', email: 'matthew@nip.com', role: 2, password: "password")
+User.create(name: 'Max King', email: 'max@nip.com', role: 2, password: "password")
+
